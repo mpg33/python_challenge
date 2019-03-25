ca2da387e585d6f545a8a32a8f65f23c74c3535b
Mike Graf <mpg@mikes-mbp.attlocal.net>
2019-03-25 11:45:18 -0700

    for Python HW

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,7 @@
 import os
 import csv
 
-csvpath = os.path.join('..'/'PyPoll'/'election_data.csv')
+csvpath = os.path.join('..', 'PyPoll', 'election_data.csv')
 
 
 candidate_List=[]
@@ -13,10 +13,9 @@ win_Percent = []
 winner_election = 0
 election_Name = ""
 
-
-with open (csvpath, newline= "") as handler:
-    pollData = csv.reader(handler)
-    for row in pollData:
+with open (csvpath, newline = "") as handler:
+	pollData = csv.reader(handler)
+	for row in pollData:
 		for row in pollData:
 			Total_VotesCast = Total_VotesCast + 1
 			candidate_List.append(row[2])
@@ -30,15 +29,21 @@ with open (csvpath, newline = "") as handler:
 			picked = candidate_List.index(picked) 
 			candidate_TotalCast[picked] += 1
 
-for row in candidateTotalCast:
-	win_Percent.append(round(row/Total_VotesCast,4))
-	#print(winPercent)
+for row in candidate_TotalCast:
+	win_Percent.append(round(row/Total_VotesCast))
+	#print(win_Percent)
+
+print("Election Results") #,"/n")
+print("--------------------")
+print(f'Total Votes: {Total_VotesCast}')
+print("--------------------")
 
 for x in range(len(candidate_TotalCast)):
 	print(str(candidate_List[x])+": "+str(win_Percent[x])+" ("+str(candidate_TotalCast[x])+")")
 	if winner_election < candidate_TotalCast[x]:
 		winner_election = candidate_TotalCast[x]
 winner_election = candidate_TotalCast.index(winner_election)
+
 	
 
 finalResult = zip(candidate_List,win_Percent,candidate_TotalCast)
@@ -50,30 +55,28 @@ finalResult = list(finalResult)
 #print("--------------------")
 #print(f'Total Votes: {Total_VotesCast}')
 #print("--------------------")
-#print(f'{candidate_List[0]}: ({win_Percent} + {candidate_TotalCast})')#in range(4)^^^^
+#print(str(candidate_List[x])+": "+str(win_Percent[x])+" ("+str(candidate_TotalCast[x])+")")
 #print("--------------------")
-#print(f'Winner: {winner_election}')
+#print(f'{finalResult}')
+print("--------------------")
+print("Winner: "+ candidate_List[winner_election])
 
 
 
+output_path = os.path.join("..", "PyPoll", "PyPoll.txt")
 
+file = open(output_path, 'w')
 
+file.write("Election Results") #,"/n")
+file.write("\n")
+file.write(f'Total Votes: {Total_VotesCast}')
+file.write("\n")
+file.write(f'{finalResult}')
+#file.write(str(candidate_List[x])+": "+str(win_Percent[x])+" ("+str(candidate_TotalCast[x])+")")
+file.write("\n")
+file.write("Winner: "+ candidate_List[winner_election])
 
-#output_path = os.path.join("..", "PyBank", "PyBank.txt")
-
-#file = open(output_path, 'w')
-
-#file.write("Financial_Analysis") #,"/n")
-#file.write("\n")
-#file.write(f'Total Months: {Number_months}')
-#file.write("\n")
-#file.write(f'Total: $ {Start_number}')
-#file.write("\n")
-#file.write(f'Greatest Increase in Profits:  {greatest_Increase[0]} + (${greatest_Increase[1]})')
-#file.write("\n")
-#file.write(f'Greatest Decrease in Profits:  {greatest_Decrease[0]} + (${greatest_Decrease[1]})')
-
-#file.close()
+file.close()
 
 
 
